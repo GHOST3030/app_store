f1693bd8320cacda0a647727d83279a7424b834c
GHOST <157970804+GHOST3030@users.noreply.github.com>
2026-04-19 22:08:16 +0300

    refactor: Comment out unused import for secret key in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:new_auth/l10n/app_localizations.dart';
-import 'package:new_auth/mykeysecret/secret.dart';
+//import 'package:new_auth/mykeysecret/secret.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'core/router/app_router.dart';
